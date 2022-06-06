649b1a64e472916147420de69250455147219599
franky3553 <franky3553@gmail.com>
2022-06-06 14:29:18 -0300

    Add createFunction2
    "createFunction2": get and set "starter2" in "actual" cell
    
    Key: 6

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ function createFunction1(event){
       let starter1 = document.getElementById("starter1").value; // - - - get starter1 text
       document.getElementById(actual).innerHTML = starter1; // - - - set starter1 text in "actual" cell
    }
-} 
+}
+
+function createFunction2(event){
+
+   if (event.keyCode == 54) {
+      let starter2 = document.getElementById("starter2").value;
+      document.getElementById(actual).innerHTML = starter2;
+   }
+}
 
 function pressReaction1A(event) {
    actual = "hab1platA"; // select "actual" cell
+
    createFunction1(event);
+   createFunction2(event);
 }
